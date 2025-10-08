378e464815ed552fcb49412d9c1c61873fbf5e08
itzAxel-code <axeldospueblos12@gmail.com>
2025-10-09 00:04:58 +0800

    main_window.py

--- app/shell/main_window.py
+++ app/shell/main_window.py
@@ -1,1 +1,35 @@
+from PyQt6.QtWidgets import (
+    QLabel,
+    QMainWindow,
+    QWidget,
+    QHBoxLayout,
+)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Car Rentals and Services")
+
+        # Central container
+        container = QWidget()
+        self.layout = QHBoxLayout(container)
+        self.setCentralWidget(container)
+
+        self.features = {}
+
+    def add_feature(self, name: str, factory):
+        # Create widget for feature
+        widget = factory()
+        self.features[name] = widget
+
+        for i in reversed(range(self.layout.count())):
+            item = self.layout.itemAt(i)
+            w = item.widget()
+            if w is not None:
+                self.layout.removeWidget(w)
+                w.deleteLater()
+
+        self.layout.addWidget(widget)
+
 
